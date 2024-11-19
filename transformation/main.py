d28e643f84069fe55f096eff57976a5ef9d73a63
Steve Rosam <steve+newdemo@quix.io>
2024-11-19 17:16:37 +0000

    Edited 'main.py'

--- transformation/main.py
+++ transformation/main.py
@@ -14,7 +14,7 @@ output_topic = app.topic(os.environ["output"])
 sdf = app.dataframe(input_topic)
 
 def schema_transformation(original_data):
-    print(original_data['value'])
+    # print(original_data['value'])
     # # Parse the JSON string into a Python dictionary
     data = json.loads(original_data['value'])
 
