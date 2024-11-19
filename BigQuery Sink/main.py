3e8482599d51bd65339f6d074d59a8469c809199
Steve Rosam <steve+newdemo@quix.io>
2024-11-19 13:41:40 +0000

    Edited 'main.py'

--- BigQuery Sink/main.py
+++ BigQuery Sink/main.py
@@ -9,14 +9,11 @@ app = Application(auto_offset_reset="earliest",
 
 topic = app.topic(os.environ["input"])
 
-# Read the service account credentials in JSON format from some environment variable.
-service_account_json = os.environ['BIGQUERY_SERVICE_ACCOUNT_JSON']
-
 TABLE_NAME = os.environ["TABLE_NAME"]
 PROJECT_ID = os.environ["PROJECT_ID"]
 DATASET_ID = os.environ["DATASET_ID"]
 DATASET_LOCATION = os.environ["DATASET_LOCATION"]
-SERVICE_ACCOUNT_JSON = os.environ["SERVICE_ACCOUNT_JSON"]
+SERVICE_ACCOUNT_JSON = os.environ["BIGQUERY_SERVICE_ACCOUNT_JSON"]
 
 # Initialize a sink
 bigquery_sink = BigQuerySink(
@@ -24,7 +21,7 @@ bigquery_sink = BigQuerySink(
     location=DATASET_LOCATION,
     dataset_id=DATASET_ID,
     table_name=TABLE_NAME,
-    service_account_json=service_account_json,
+    service_account_json=SERVICE_ACCOUNT_JSON,
     schema_auto_update=True,
     ddl_timeout=10.0,
     insert_timeout=10.0,
