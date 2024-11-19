5375f97b03cf1e1e881e04272c1a3575c15c835b
Steve Rosam <steve+newdemo@quix.io>
2024-11-19 17:17:44 +0000

    Edited 'main.py'

--- transformation/main.py
+++ transformation/main.py
@@ -16,8 +16,8 @@ sdf = app.dataframe(input_topic)
 def schema_transformation(original_data):
     # print(original_data['value'])
     # # Parse the JSON string into a Python dictionary
-    data = json.loads(original_data['value'])
-    print(data)
+    data = json.loads(original_data)
+    print(data['value'])
 
     # # Change 'm' to 'field' and 'mem' to 'memory'
     # if 'm' in data:
