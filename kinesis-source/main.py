ec2ffe176280a0c49fbb0e1ed0fb6d3b119048b6
Steve Rosam <steve+newdemo@quix.io>
2024-11-20 13:49:53 +0000

    Edited 'main.py'

--- kinesis-source/main.py
+++ kinesis-source/main.py
@@ -4,3 +4,16 @@ from kinesis_source import KinesisSource
 import os
 from dotenv import load_dotenv
 
+load_dotenv() # load environment variables
+
+kinesis_source = KinesisSource()
+
+app = Application() # Quix Streams App class
+
+sdf = app.dataframe(source=kinesis_source)
+
+sdf.print()
+
+if __name__ == "__main__":
+    app.run()
+
