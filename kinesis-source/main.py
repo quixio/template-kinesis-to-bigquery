a56e52763e28554256736e48031446078995be88
Steve Rosam <steve+newdemo@quix.io>
2024-11-20 13:55:26 +0000

    Edited 'main.py'

--- kinesis-source/main.py
+++ kinesis-source/main.py
@@ -5,8 +5,17 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv() # load environment variables
+kinesis_stream = os.environ["kinesis_stream"]
 
-kinesis_source = KinesisSource()
+kinesis_source = KinesisSource(
+    name=kinesis_stream,
+    stream_name=kinesis_stream,
+    aws_access_key_id=os.environ["aws_access_key_id"],
+    aws_secret_access_key=os.environ["aws_secret_access_key"],
+    region_name=os.environ["aws_region_name"],
+    endpoint_url="http://kinesis:4566"  
+
+)
 
 app = Application() # Quix Streams App class
 
