6f97ba6b0cdfbb5a58da4a7ce4ba49aeac31475f
Steve Rosam <steve+newdemo@quix.io>
2024-11-21 10:16:34 +0000

    Edited 'main.py'

--- BigQuery Sink/main.py
+++ BigQuery Sink/main.py
@@ -9,19 +9,13 @@ app = Application(auto_offset_reset="earliest",
 
 topic = app.topic(os.environ["input"])
 
-TABLE_NAME = os.environ["TABLE_NAME"]
-PROJECT_ID = os.environ["PROJECT_ID"]
-DATASET_ID = os.environ["DATASET_ID"]
-DATASET_LOCATION = os.environ["DATASET_LOCATION"]
-SERVICE_ACCOUNT_JSON = os.environ["BIGQUERY_SERVICE_ACCOUNT_JSON"]
-
 # Initialize a sink
 bigquery_sink = BigQuerySink(
-    project_id=PROJECT_ID,
-    location=DATASET_LOCATION,
-    dataset_id=DATASET_ID,
-    table_name=TABLE_NAME,
-    service_account_json=SERVICE_ACCOUNT_JSON,
+    project_id=os.environ["PROJECT_ID"],
+    location=os.environ["DATASET_LOCATION"],
+    dataset_id=os.environ["DATASET_ID"],
+    table_name=os.environ["TABLE_NAME"],
+    service_account_json=os.environ["BIGQUERY_SERVICE_ACCOUNT_JSON"],
     schema_auto_update=True,
     ddl_timeout=10.0,
     insert_timeout=10.0,
