0bfc327f7f2dfa1eb77cbbcdbf03b6af9e3a5903
Steve Rosam <steve+newdemo@quix.io>
2024-11-21 10:20:21 +0000

    Edited 'main.py'

--- publish-kinesis/main.py
+++ publish-kinesis/main.py
@@ -65,7 +65,8 @@ while True:
     }
 
     data_json = json.dumps(data_dict)
-
+    print("Publishing:")
+    print(data_json)
     kinesis_client.put_record(
         StreamName=stream_name,
         Data=data_json.encode('utf-8'),
